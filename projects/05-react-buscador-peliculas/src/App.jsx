f3a546cc05a8968619ba5f8cfdc4c10bc5c4d210
AnoraDragoon <anoradragoon@gmail.com>
2025-08-28 15:59:19 +0200

    Init 05 buscador de pelis

---
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form">
+          <input type="text" placeholder="Avengers, Star War, The Matrix..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>Aquí irán las películas</main>
+    </div>
+  );
+}
+
+export default App;
