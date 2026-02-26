8c98e063ed882f56e35e69e92552493e753ac7b2
AnoraDragoon <anoradragoon@gmail.com>
2026-02-26 11:14:12 +0100

    SPA with navigation

--- projects/07-midu-router/src/App.jsx
+++ projects/07-midu-router/src/App.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { EVENTS } from "./utils/consts";
 
-// const NAVIGATION_EVENT = "pushstate";
-
-// function navigate(href) {
-//   window.history.pushState({}, "", href);
-//   const navigationEvent = new Event(NAVIGATION_EVENT);
-//   window.dispatchEvent(navigationEvent);
-// }
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(EVENTS.PUSHSTATE);
+  window.dispatchEvent(navigationEvent);
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <p>Esta es la página principal. Crear un reactrouter desde cero.</p>
-      <a href="/about">Ir a Sobre nosotros</a>
+      <button onClick={() => navigate("/about")}>Ir a Sobre nosotros</button>
     </>
   );
 }
@@ -24,7 +23,7 @@ function AboutPage() {
       <h1>About</h1>
       <img src="/vite.svg" alt="Logo" />
       <p>¡Hola! Me llamo Osvaldo y estoy creando un clone de React Router.</p>
-      <a href="/">Ir al Home</a>
+      <button onClick={() => navigate("/")}>Ir al Home</button>
     </>
   );
 }
@@ -32,14 +31,18 @@ function AboutPage() {
 function App() {
   const [currentPath, setCurrentPath] = useState(globalThis.location.pathname);
 
-  // useEffect(() => {
-  //   const onLocationChange = () => {
-  //     setCurrentPath(window.location.pathname);
-  //   };
-  //   window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange);
+    window.addEventListener(EVENTS.POPSTATE, onLocationChange);
 
-  //   return () => window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
-  // }, []);
+    return () => {
+      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange);
+      window.removeEventListener(EVENTS.POPSTATE, onLocationChange);
+    };
+  }, []);
 
   return (
     <main>
